d9a98650c3377b4f710c3942bc5acb93ba505af9
Andras Corda <115708078+R3tr0-andras@users.noreply.github.com>
2024-04-08 17:59:50 +0200

    Add files via upload

---
+++ stockage d'adresses IP/GestionAdressesIP.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stockage_d_adresses_IP
+{
+    public struct GestionAdressesIP
+    {
+        private const int MAX_ADRESSES = 20;
+        private const int MAX_NOMS = 20;
+        private static int[,] adressesIP = new int[MAX_ADRESSES, 4];
+        private static string[] noms = new string[MAX_NOMS];
+        private static int nbAdresses = 0;
+        private static int nbNoms = 0;
+
+        public static bool LireOctet(out int octet)
+        {
+            octet = 0;
+            Console.Write("Entrez un nombre entre 0 et 255 : ");
+            if (int.TryParse(Console.ReadLine(), out octet) && octet >= 0 && octet <= 255)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static void LireAdresseIP()
+        {
+            if (nbAdresses < MAX_ADRESSES)
+            {
+                int[] adresse = new int[4];
+                for (int i = 0; i < 4; i++)
+                {
+                    int octet;
+                    do
+                    {
+                        Console.WriteLine($"Entrez l'octet {i + 1} de l'adresse IP :");
+                    } while (!LireOctet(out octet));
+                    adresse[i] = octet;
+                }
+                for (int i = 0; i < 4; i++)
+                {
+                    adressesIP[nbAdresses, i] = adresse[i];
+                }
+                nbAdresses++;
+            }
+            else
+            {
+                Console.WriteLine("La matrice est pleine, impossible d'ajouter une nouvelle adresse.");
+            }
+        }
+
+        public static bool AjouteAdresseIP()
+        {
+            if (nbAdresses < MAX_ADRESSES)
+            {
+                LireAdresseIP();
+                return true;
+            }
+            return false;
+        }
+
+        public static bool AjouteNom(string nom)
+        {
+            if (nbNoms < MAX_NOMS)
+            {
+                noms[nbNoms] = nom;
+                nbNoms++;
+                return true;
+            }
+            return false;
+        }
+
+        public static string ConcateneAdresse(int ligne)
+        {
+            string adresseConcatenee = "";
+
+            for (int i = 0; i < 4; i++)
+            {
+                adresseConcatenee += adressesIP[ligne, i];
+
+                if (i < 3)
+                {
+                    adresseConcatenee += ".";
+                }
+            }
+
+            return adresseConcatenee;
+        }
+
+        public static string ConcateneTout()
+        {
+            string result = "";
+
+            for (int i = 0; i < nbNoms; i++)
+            {
+                string adresse = ConcateneAdresse(i);
+                result += $"{noms[i]} : {adresse}\n";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ stockage d'adresses IP/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace stockage_d_adresses_IP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continuer = true;
+
+            while (continuer)
+            {
+                AfficherMenu();
+                string choix = Console.ReadLine();
+
+                switch (choix)
+                {
+                    case "1":
+                        GestionAdressesIP.AjouteAdresseIP();
+                        break;
+                    case "2":
+                        Console.WriteLine("Entrez le nom associé à l'adresse IP :");
+                        string nom = Console.ReadLine();
+                        GestionAdressesIP.AjouteNom(nom);
+                        break;
+                    case "3":
+                        Console.WriteLine(GestionAdressesIP.ConcateneTout());
+                        break;
+                    case "stop":
+                        continuer = false;
+                        break;
+                    default:
+                        Console.WriteLine("Choix invalide. Veuillez réessayer.");
+                        break;
+                }
+            }
+        }
+
+        static void AfficherMenu()
+        {
+            Console.WriteLine("=== Gestion des Adresses IP ===");
+            Console.WriteLine("1. Ajouter une adresse IP");
+            Console.WriteLine("2. Ajouter un nom à une adresse IP");
+            Console.WriteLine("3. Afficher toutes les adresses IP avec leurs noms");
+            Console.WriteLine("Tapez 'stop' pour quitter et afficher les résultats");
+            Console.Write("Choix : ");
+        }
+    }
+}
